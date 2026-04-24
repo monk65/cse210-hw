9212fbe697b3b20587affabd5718b5b7330591a7
monk65 <stokesorion@gmail.com>
2026-04-24 15:21:08 -0600

    Added grading code that declares letter grade and if you passed or not

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade:");
+        string num = Console.ReadLine();
+        string letter = "F";
+        int grade = int.Parse(num);
+        if (grade >= 90)
+        {
+           letter = "A";
+        }
+        else if (grade >= 80 && grade < 90 )
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80 )
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70 )
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("Your grade is undefined");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine($"You passed the class with a {letter} with a {grade}%");
+        }
+        else
+            Console.WriteLine($"You Failed the class with a {letter} with a {grade}% ");
+    }  
 }
