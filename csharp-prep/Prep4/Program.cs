3fd5745ff1e6852d153465aa6660b4640a7e56a1
monk65 <stokesorion@gmail.com>
2026-05-02 15:58:15 -0600

    made code that would make a list adding numbers and when 0 is typed it moves to finding the sum and the average and the larget numbr

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        Console.Write("Enter number: ");
+        int input = int.Parse(Console.ReadLine());
+
+        while (input != 0)
+        {
+            numbers.Add(input);
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        double average = (double)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
