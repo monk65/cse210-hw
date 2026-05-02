bcca346682a8a61e479950eba8fc2203ba8c4bf1
monk65 <stokesorion@gmail.com>
2026-05-02 15:40:41 -0600

    I replaced hello world with number generation game that makes you guess

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+    Random randomGenerator = new Random();
+int mNumber = randomGenerator.Next(1, 101);
+
+int guess;
+
+do
+{    
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (guess < mNumber)
+    {
+        Console.WriteLine("Higher");
+    }
+    else if (guess > mNumber)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine("You guessed the magic number!");
+    }
+} while (guess != mNumber);
     }
 }
---
+++ sandbox/Sandbox/sandbox2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");// hello world 
+    }
+}
