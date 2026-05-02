2a610d73a8af1c416e617232c6e5d6463733cdcc
monk65 <stokesorion@gmail.com>
2026-05-02 16:24:45 -0600

    made code that holds many function that record name records Favorite number records year born a function squares favorite number subtracts brith day year from this year to find age

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+
+    static void DisplayWelcome()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the program!");
     }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write("Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squared, int birthYear)
+    {
+        int age = 2026 - birthYear;
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+        Console.WriteLine($"{name}, you will turn {age} this year.");
+    }
+static void Main(string[] args)
+{
+    DisplayWelcome();
+    string name = PromptUserName();
+    int number = PromptUserNumber();
+    PromptUserBirthYear(out int birthYear);
+    int squared = SquareNumber(number);
+    DisplayResult(name, squared, birthYear);
+}
+
 }
