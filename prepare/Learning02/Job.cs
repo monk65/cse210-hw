e1624afee5b15720838bbde7c84b2b49e845357b
monk65 <stokesorion@gmail.com>
2026-05-03 19:24:20 -0600

    made classes for job it held company there job title there start year and end year I made class for resume that held persons name and list for job, then I made the main program test the class by filing them and calling the resume display.

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,14 @@
+using System;
+public class Job
+{
+    public String _company ="";
+    public string _jobTitle = "";
+    public int _startYear = 0;
+    public int _endYear = 0;
+    public Job()
+    {}
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,20 @@
+using System;
+public class Resume
+{
+    public String _name ="";
+    public List<Job> _jobs = new List<Job>();
+        public Resume()
+    {
+        
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name:{_name}");
+        Console.WriteLine($"Jobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    }
+}
+
