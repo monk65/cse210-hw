d09220b69bff2296ba07259b4ddb70b367546559
monk65 <stokesorion@gmail.com>
2026-05-18 11:55:53 -0600

    make a fraction that take a top and bottom of a fraction and runs it in to deffrent fuction to get the fraction and the decimal of the fraction I also make it so it could crash when it devideds by Zero by making the zero = 1. then I had it run through the example nujmber to make sure it works

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        Console.WriteLine();
+        Random random = new Random();
+        Fraction f = new Fraction();
+
+        for (int i = 0; i < 20; i++)
+        {
+            f.SetTop(random.Next(1, 11));
+            f.SetBottom(random.Next(1, 11));
+            Console.WriteLine($"Fraction {i + 1}: string: {f.GetFractionString()} Number: {f.GetDecimalValue()}");
+        }
+
     }
 }
